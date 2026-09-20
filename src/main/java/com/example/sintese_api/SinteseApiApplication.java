22ce9829d8bbac63156e8daae87b4f8f839d07dd
Isaque Carlos da Silva Melo <isaquecarlos2016@gmail.com>
2026-09-19 21:44:17 -0300

    initializing project

---
+++ src/main/java/com/example/sintese_api/SinteseApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.sintese_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinteseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinteseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sintese_api/SinteseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sintese_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinteseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
