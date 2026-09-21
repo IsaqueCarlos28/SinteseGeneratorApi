fe77108323e04cfd61bd15bd51ddef7f2369af5a
Isaque Carlos da Silva Melo <isaquecarlos2016@gmail.com>
2026-09-20 21:13:12 -0300

    chore: cleaning double import

--- src/test/java/com/example/sintese_api/controller/SinteseControllerTest.java
+++ src/test/java/com/example/sintese_api/controller/SinteseControllerTest.java
@@ -12,7 +12,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(SinteseController.class)
 class SinteseControllerTest {
